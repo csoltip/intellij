edfd4441ad6c09534c7efc5031863ecd1f32b244
CsoltiP <csolti@szamalk-szalezi.hu>
2024-10-05 08:56:56 +0200

    peldanyositas

--- src/main/java/hu/soter/Main.java
+++ src/main/java/hu/soter/Main.java
@@ -2,10 +2,11 @@ package hu.soter;
 
 public class Main {
     public static void main(String[] args) {
-        int i = new Main().fv();
+        Main m = new Main();
+        int i = m.fv();
     }
 
     private int fv() {
-        return 0;
+        return 1;
     }
 }
