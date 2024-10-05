677602bea3044a50c0a21bc3110282543b237f28
CsoltiP <csolti@szamalk-szalezi.hu>
2024-10-05 08:55:23 +0200

    int fv()

--- src/main/java/hu/soter/Main.java
+++ src/main/java/hu/soter/Main.java
@@ -2,10 +2,10 @@ package hu.soter;
 
 public class Main {
     public static void main(String[] args) {
-        int i = fv();
+        int i = new Main().fv();
     }
 
-    private static int fv() {
+    private int fv() {
         return 0;
     }
 }
